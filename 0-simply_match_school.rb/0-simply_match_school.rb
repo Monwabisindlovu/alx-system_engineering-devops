93c3a2bd6bb47534b7f442ba26537cdf025eb021
monwabisindlovu <monwabisindlovu78@gmail.com>
2023-10-31 03:30:12 -0700

    0-simply_match_school.rb

--- 0-simply_match_school.rb/0-simply_match_school.rb
+++ 0-simply_match_school.rb/0-simply_match_school.rb
@@ -1,19 +1,20 @@
 #!/usr/bin/env ruby
 
-# Check if an argument is passed
+""" Check if an argument is passed """
 if ARGV.length != 1
   puts "Usage: #{$PROGRAM_NAME} <text>"
   exit 1
 end
 
-# Regular expression to match "School"
+""" Regular expression to match "School" using Oniguruma """
 regex = /School/
 
-# Input text from the command line argument
+""" Input text from the command line argument """
 input_text = ARGV[0]
 
-# Match the regular expression in the input text
+""" Match the regular expression in the input text """
 match = input_text.scan(regex)
 
-# Print the matched result
+""" Print the matched result """
 puts match.join
+
