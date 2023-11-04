aca231a7b4f74b7678fc54d610cc04ad1dc99e4b
monwabisindlovu <monwabisindlovu78@gmail.com>
2023-11-03 22:56:59 -0700

    2-repetition_token_1.rb

--- 0x06-regular_expressions/2-repetition_token_1.rb
+++ 0x06-regular_expressions/2-repetition_token_1.rb
@@ -1,16 +1,2 @@
 #!/usr/bin/env ruby
-
-# Check if an argument is passed
-if ARGV.length != 1
-  puts "Usage: #{$PROGRAM_NAME} <text>"
-  exit 1
-end
-
-# Regular expression to match "hbn" with 'hbt' or 'hbtn' preceding it
-regex = /hbt+n/
-
-# Input text from the command line argument
-input_text = ARGV[0]
-
-# Match the regular expression in the input text
-match = input_text.scan(regex)
+puts ARGV[0].scan(/hb?t?n/).join
