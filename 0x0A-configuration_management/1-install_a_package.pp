fcc703b387d4f526c56143538d871a1f1f9d86a5
monwabisindlovu <monwabisindlovu78@gmail.com>
2023-12-22 05:35:10 -0800

    1-install_a_package.pp

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,14 +1,13 @@
 # Puppet manifest to execute a command to kill a process named killmenow
 
 # Define the exec resource to kill the process
-exec { 'killmenow':
-  command     => 'pkill -f killmenow',
-  path        => '/usr/bin:/bin',  # Add any necessary paths
-  refreshonly => true,             # Only run the command if the exec is notified
+package { 'python3-pip':
+  ensure => installed,
 }
 
-# Notify the exec resource to run and kill the process
-notify { 'Kill process killmenow':
-  require => Exec['killmenow'],
+package { 'Flask':
+  ensure   => '2.1.0',
+  provider => 'pip3',
+  require  => Package['python3-pip'],
 }
 
