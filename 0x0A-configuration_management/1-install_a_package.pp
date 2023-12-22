3c3bd56d9017df687dcb9590ecfbb22ad1733a4a
monwabisindlovu <monwabisindlovu78@gmail.com>
2023-12-22 04:46:39 -0800

    1-install_a_package.pp

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,14 +1,5 @@
-# Puppet manifest to execute a command to kill a process named killmenow
-
-# Define the exec resource to kill the process
-exec { 'killmenow':
-  command     => 'pkill -f killmenow',
-  path        => '/usr/bin:/bin',  # Add any necessary paths
-  refreshonly => true,             # Only run the command if the exec is notified
+# installing flask package using puppet
+package { 'flask':
+  ensure   => '2.1.0',
+  provider => 'pip3',
 }
-
-# Notify the exec resource to run and kill the process
-notify { 'Kill process killmenow':
-  require => Exec['killmenow'],
-}
-
