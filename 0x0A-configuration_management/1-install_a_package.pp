6aa6413c1c9d9efafb4017828de7434c4bb89eac
monwabisindlovu <monwabisindlovu78@gmail.com>
2023-12-22 09:35:20 -0800

    1-install_a_package.pp

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,11 +1,27 @@
 #!/usr/bin/pup
-#Puppet manifest to install Flask using pip3
+# Puppet manifest to install Flask version 2.1.0 using pip3
+
+# Ensure python3-pip is installed (dependency for pip3)
+package { 'python3-pip':
+  ensure => 'installed',
+}
+
+# Define package installation for Flask
+package { 'Flask':
+  ensure   => '2.1.0',
+  provider => 'pip3',
+  require  => Package['python3-pip'],  # Ensure pip3 is installed before attempting to install Flask
+}
+
+# Define exec resource to kill a process named 'killmenow'
 exec { 'killmenow':
   command     => 'pkill -f killmenow',
   path        => '/usr/bin:/bin',
   refreshonly => true,
 }
 
-  notify { 'Kill process killmenow':
+# Notify the exec resource to run and kill the process
+notify { 'Kill process killmenow':
   require => Exec['killmenow'],
 }
+
