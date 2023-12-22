bfcbaf1bc821b6be0acb5a3248165bb76da308de
monwabisindlovu <monwabisindlovu78@gmail.com>
2023-12-22 05:57:37 -0800

    1-install_a_package.pp

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,6 +1,11 @@
 #!/usr/bin/pup
 #Puppet manifest to install Flask using pip3
-package {'flask':
-  ensure   => '2.1.0',
-  provider => 'pip3'
+exec { 'killmenow':
+  command     => 'pkill -f killmenow',
+  path        => '/usr/bin:/bin',
+  refreshonly => true,
+}
+
+  notify { 'Kill process killmenow':
+  require => Exec['killmenow'],
 }
