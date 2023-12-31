edb2e6d3e234ffc6756c19cde62e667c4fd2dd39
monwabisindlovu <monwabisindlovu78@gmail.com>
2023-12-31 06:05:27 -0800

    100-puppet_ssh_config.pp

--- 0x0B-ssh/100-puppet_ssh_config.pp
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -2,15 +2,15 @@
 
 # Turn off password authentication
 file_line { 'Turn off passwd auth':
-  path   => '/root/.ssh/config',
-  line   => 'PasswordAuthentication no',
-  match  => '^#?PasswordAuthentication',
+  path  => '/root/.ssh/config',
+  line  => 'PasswordAuthentication no',
+  match => '^#?PasswordAuthentication',
 }
 
 # Declare identity file
 file_line { 'Declare identity file':
-  path   => '/root/.ssh/config',
-  line   => 'IdentityFile ~/.ssh/school',
-  match  => '^#?IdentityFile',
+  path  => '/root/.ssh/config',
+  line  => 'IdentityFile ~/.ssh/school',
+  match => '^#?IdentityFile',
 }
 
