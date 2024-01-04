412518cf9ab3520f46b21cb57e8cd1a71a6c542e
monwabisindlovu <monwabisindlovu78@gmail.com>
2024-01-04 05:39:14 -0800

    7-puppet_install_nginx_web_server.pp

--- 0x0C-web_server/7-puppet_install_nginx_web_server.pp
+++ 0x0C-web_server/7-puppet_install_nginx_web_server.pp
@@ -1,5 +1,38 @@
 #!/usr/bin/env bash
 # upload configuration file to a server
+class nginx_install {
+    package { 'nginx':
+        ensure => installed,
+    }
 
-scp ~/AJIyanu/alx-system_engineering-devops/0x0F-load_balancer/7-puppet_install_nginx_web_server.pp ubuntu@$1:~/
-ssh ubuntu@$1 'bash -s' < config.sh}
+    service { 'nginx':
+        ensure => running,
+        enable => true,
+    }
+
+    file { '/var/www/html/index.nginx-debian.html':
+        ensure  => file,
+        content => 'Hello World!',
+    }
+
+    file { '/etc/nginx/sites-available/default':
+        ensure  => file,
+        source  => 'puppet:///modules/nginx/default',
+    }
+}
+
+class nginx_redirect {
+    file { '/etc/nginx/sites-available/redirect':
+        ensure  => file,
+        source  => 'puppet:///modules/nginx/redirect',
+    }
+
+    file { '/etc/nginx/sites-enabled/redirect':
+        ensure => link,
+        target => '/etc/nginx/sites-available/redirect',
+    }
+}
+
+include nginx_install
+include nginx_redirect
+
