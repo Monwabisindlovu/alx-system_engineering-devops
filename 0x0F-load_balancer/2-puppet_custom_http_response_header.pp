1ff6ce000c16028c561ebd796b6b40a3478aba56
monwabisindlovu <monwabisindlovu78@gmail.com>
2024-01-08 05:54:09 -0800

    2-puppet_custom_http_response_header.pp

--- 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
+++ 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
@@ -1,8 +1,37 @@
 #/bin/bash
 chmod u+x *
-chmod 644 README.md
-#chmod 644 2-puppet_custom_http_response_header.pp
+# this will automate the previous task i've done(creating custom header for the http response)
+exec { 'update':
+  command => '/usr/bin/apt-get update',
+}
 
-git add .
-git commit -m "Load Balancer"
-git push
+package { 'nginx':
+  ensure  => 'present',
+  require => Exec['update'],
+}
+
+file_line { '404':
+  ensure  => 'present',
+  path    => '/etc/nginx/sites-enabled/default',
+  after   => 'listen 80 default_server;',
+  line    => 'rewrite ^/redirect_me https://www.youtube.com/watch?v=QH2-TGUlwu4 permanent;',
+  require => Package['nginx'],
+}
+
+file_line { 'add_header':
+  ensure  => 'present',
+  path    => '/etc/nginx/sites-enabled/default',
+  after   => 'listen80 default_server;',
+  line    => 'add_header X-Served-By $hostname;',
+  require => Package['nginx'],
+}
+
+file { '/var/www/html/index.html':
+  content => 'Holberton School',
+  require => Package['nginx'],
+}
+
+service { 'nginx':
+  ensure  => running,
+  require => Package['nginx'],
+}
