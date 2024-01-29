8f816f573cb1ae56b98d01943e7223aeb8a5f605
monwabisindlovu <monwabisindlovu78@gmail.com>
2024-01-29 02:23:15 -0800

    1-export_to_CSV.py

--- 0x15-api/1-export_to_CSV.py
+++ 0x15-api/1-export_to_CSV.py
@@ -5,50 +5,29 @@ Module to export data to CSV.
 
 import csv
 import requests
-from sys import argv
 
-def get_employee_data(employee_id):
-    """
-    Function to get employee data from the specified API endpoint.
-    """
-    base_url = "https://jsonplaceholder.typicode.com"
-    user_url = f"{base_url}/users/{employee_id}"
-    todos_url = f"{base_url}/todos?userId={employee_id}"
+"""Get the user ID from the command line argument"""
+user_id = sys.argv[1]
 
-    user_response = requests.get(user_url)
-    todos_response = requests.get(todos_url)
+""" Get the user information from the API"""
+user_url = "https://jsonplaceholder.typicode.com/users/{}".format(user_id)
+user_response = requests.get(user_url)
+user_data = user_response.json()
+user_name = user_data.get("username")
 
-    user_data = user_response.json()
-    todos_data = todos_response.json()
+"""Get the tasks information from the API"""
+tasks_url = "https://jsonplaceholder.typicode.com/todos?userId={}".format(user_id)
+tasks_response = requests.get(tasks_url)
+tasks_data = tasks_response.json()
 
-    return user_data, todos_data
-
-def export_to_csv(employee_id, user_data, todos_data):
-    """
-    Function to export data to CSV format.
-    """
-    employee_name = user_data.get("username")
-    file_name = f"{employee_id}.csv"
-
-    with open(file_name, mode='w', newline='') as csv_file:
-        fieldnames = ["USER_ID", "USERNAME", "TASK_COMPLETED_STATUS", "TASK_TITLE"]
-        writer = csv.DictWriter(csv_file, fieldnames=fieldnames)
-
-        writer.writeheader()
-        for task in todos_data:
-            writer.writerow({
-                "USER_ID": employee_id,
-                "USERNAME": employee_name,
-                "TASK_COMPLETED_STATUS": str(task.get("completed")),
-                "TASK_TITLE": task.get("title")
-            })
-
-if __name__ == "__main__":
-    if len(argv) != 2:
-        print("Usage: {} <employee_id>".format(argv[0]))
-        exit(1)
-
-    employee_id = int(argv[1])
-    user_data, todos_data = get_employee_data(employee_id)
-    export_to_csv(employee_id, user_data, todos_data)
-
+"""Create a CSV file with the user ID as the name"""
+file_name = "{}.csv".format(user_id)
+with open(file_name, "w", newline="") as csv_file:
+    """Create a CSV writer object"""
+    csv_writer = csv.writer(csv_file, quoting=csv.QUOTE_ALL)
+    """Loop through the tasks data and write each row"""
+    for task in tasks_data:
+        task_id = task.get("id")
+        task_title = task.get("title")
+        task_completed = task.get("completed")
+        csv_writer.writerow([user_id, user_name, task_completed, task_title])
