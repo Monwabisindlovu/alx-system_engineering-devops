bef72e775365a2e460700fc42fa9170a9db2d211
monwabisindlovu <monwabisindlovu78@gmail.com>
2024-02-14 13:40:22 +0000

    0-strace_is_your_friend.pp

--- 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
+++ 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
@@ -1,7 +1,6 @@
 # Corrects a typo in wordpress settings and fixes 500 error
-exec { 'fix-wordpress-typo':
+exec { 'fix-apache':
   command => "sed -i 's/phpp/php/g' /var/www/html/wp-settings.php",
-  onlyif  => '/usr/bin/test -f /var/www/html/wp-settings.php',
-  require => Package['php'],
+  path    => ['/bin', '/usr/bin'],
 }
 
