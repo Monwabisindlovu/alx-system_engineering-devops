e580c415cb574b79101a88eb9de00dd9ce6c5606
monwabisindlovu <monwabisindlovu78@gmail.com>
2024-02-14 13:29:25 +0000

    0-strace_is_your_friend.pp

--- 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
+++ 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
@@ -1,13 +1,4 @@
 # Corrects a typo in wordpress settings and fixes 500 error
-file { '/var/www/html/wp-settings.php':
-  ensure => file,
-  mode   => '0644',
-  source => 'puppet:///modules/wordpress/wp-settings.php',
-}
-
-exec { 'correct_typo':
-  command     => "sed -i 's/phpp/php/g' /var/www/html/wp-settings.php",
-  path        => '/bin',
-  refreshonly => true,
-  subscribe   => File['/var/www/html/wp-settings.php'],
+exec { 'fix_typo':
+  command => "sed -i 's/phpp/php/g' /var/www/html/wp-settings.php",
 }
