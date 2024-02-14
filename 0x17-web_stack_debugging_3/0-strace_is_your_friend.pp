2c9d56152164f849377fc5dedc7230d45f933401
monwabisindlovu <monwabisindlovu78@gmail.com>
2024-02-14 13:15:13 +0000

    0-strace_is_your_friend.pp

--- 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
+++ 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
@@ -1,7 +1,5 @@
 # Corrects a typo in wordpress settings and fixes 500 error
-exec { 'fix-wordpress-typo':
-  command => "sed -i 's/phpp/php/g' /var/www/html/wp-settings.php",
-  onlyif  => '/usr/bin/test -f /var/www/html/wp-settings.php',
-  require => Package['php'],
+exec { 'fix-wordpress':
+  command => 'sed -i "s/Holberton &#8211;/Holberton -/g" /var/www/html/wp-content/themes/twentyseventeen/header.php',
+  onlyif => 'grep -q "Holberton &#8211;" /var/www/html/wp-content/themes/twentyseventeen/header.php',
 }
-
