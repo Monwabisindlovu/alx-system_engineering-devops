0360e83f073c8bc37aee53a2571426f9a6378ca4
monwabisindlovu <monwabisindlovu78@gmail.com>
2024-02-14 12:28:27 +0000

    0-strace_is_your_friend.pp

--- 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
+++ 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
@@ -2,6 +2,7 @@
 
 exec { 'fix-wordpress':
   command => 'chown -R www-data:www-0 /var/lib/php/sessions && chmod -R 770 /var/lib/php/sessions',
-  onlyif  => 'test -d /var/lib/php/sessions',
+  onlyif  => '/usr/bin/test -d /var/lib/php/sessions',
   require => Package['php'],
 }
+
