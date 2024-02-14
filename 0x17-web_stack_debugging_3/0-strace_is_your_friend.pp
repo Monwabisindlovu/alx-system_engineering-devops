13d8c1e8874707b14616f7c1e76324e3f40e70cc
monwabisindlovu <monwabisindlovu78@gmail.com>
2024-02-14 12:19:31 +0000

    0-strace_is_your_friend.pp

--- 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
+++ 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
@@ -1,10 +1,7 @@
 # This Puppet manifest fixes the issue causing Apache to return a 500 error
-# by ensuring the directory /var/lib/php/sessions exists.
 
 exec { 'fix-wordpress':
-  command  => 'mkdir -p /var/lib/php/sessions',
-  onlyif   => 'test -d /var/lib/php/sessions',
-  path     => '/bin:/usr/bin',
-  provider => shell,
+  command => 'chown -R www-data:www-0 /var/lib/php/sessions && chmod -R 770 /var/lib/php/sessions',
+  onlyif  => 'test -d /var/lib/php/sessions',
+  require => Package['php'],
 }
-
